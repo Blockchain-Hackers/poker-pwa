8817ace63450d6cb283dbc81e1ba2aa7bf29d0a8
Favour <gismozfexie@yahoo.com>
2024-01-28 22:06:18 +0100

    chore: add submit button + minor mod

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -82,41 +82,56 @@ const switchTab = (tab: 'login' | 'signup') => {
     activeTab: tab
   }
 }
+
+const submitForm = () => {
+  console.log(form.value.form)
+}
 </script>
 
 <template>
   <div
     class="min-h-[100svh] flex flex-col justify-center items-center px-6
     [background-image:_linear-gradient(to_bottom_right,_rgba(0,0,0,.6),_rgba(0,0,0,.2)),url('@/assets/images/Site-background-dark.webp')] [background-size:cover]">
-    <div
-      class="bg-white w-[500px] max-w-full rounded-xl ring-4 ring-amber-800 text-xl
-      [background-image:url('@/assets/images/bg-texture.jpg')] [background-size:100px]">
-      <ul class="w-full flex gap-6 px-4 md:px-6 justify-center mt-6 font-black text-amber-900">
+    <div class="grid gap-6 w-[500px] max-w-full">
+      <ul class="w-full flex gap-6 justify-center mt-6 font-black text-amber-900">
         <li
           v-for="tab in ['login', 'signup']"
           @click="switchTab(tab as 'login' | 'signup')"
           :class="{ 'is-active flex-grow !border-white/90 text-white': form.activeTab === tab }"
-          class="cursor-pointer p-2 px-4 bg-amber-700 rounded-lg
-          transition-all duration-300 border-4 border-transparent uppercase">
+          class="cursor-pointer p-2 px-4 bg-amber-700 rounded-lg shadow-2xl shadow-black
+          transition-all duration-300 border-4 border-black/10 uppercase">
           {{ tab }}
         </li>
       </ul>
+      
+      <div
+        class="bg-white rounded-xl ring-4 ring-amber-800 text-xl shadow-2xl shadow-black
+        [background-image:url('@/assets/images/bg-texture.jpg')] [background-size:100px]">
+        <form id="form" @submit.prevent="submitForm" class="flex flex-col p-4 md:p-6 gap-y-6 font-black">
+          <template v-for="input in form.form" :key="input.name">
+            <label class="">
+              <span class="block text-white px-1">{{ input.label }}</span>
+              <input
+                :name="input.name"
+                :type="input.type"
+                :placeholder="input.placeholder"
+                v-model="input.modelValue"
+                class="w-full border-4 border-solid border-black/30 bg-amber-700/90
+                rounded-xl p-2 outline-none text-white placeholder:text-amber-800"
+              />
+            </label>
+          </template>
+        </form>
+      </div>
 
-      <form class="flex flex-col p-4 md:p-6 gap-y-6 font-black">
-        <template v-for="input in form.form" :key="input.name">
-          <label class="">
-            <span class="block text-white px-1">{{ input.label }}</span>
-            <input
-              :name="input.name"
-              :type="input.type"
-              :placeholder="input.placeholder"
-              v-model="input.modelValue"
-              class="w-full border-4 border-solid border-black/30 bg-amber-700/90
-              rounded-xl p-2 outline-none text-white placeholder:text-amber-800"
-            />
-          </label>
-        </template>
-      </form>
+      <button
+        form="form"
+        class="bg-amber-700/90 text-white rounded-lg p-2
+        font-bold shadow-2xl shadow-black outline-none
+        ring-4 ring-amber-800 hover:ring-white/90
+        transition-all duration-300 uppercase">
+        {{ form.activeTab }}
+      </button>
     </div>
   </div>
 </template>
